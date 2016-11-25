cb392c6ad419c5d9542536be5bfda5bb4f8b618b
Thunder33345 <Thunder33345@users.noreply.github.com>
2016-11-25 19:17:39 +0800

    clean up

--- src/Thunder33345/CommandBlocker/Main.php
+++ src/Thunder33345/CommandBlocker/Main.php
@@ -10,85 +10,87 @@ use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase implements Listener
 {
-	public
-		$blockConsole = true,
-		$blockRemote = true;
-	protected
-		$whiteListedPlayer = [],
-		$blockedCommands = [],
-		$logFile = null;
+  public
+    $blockConsole = true,
+    $blockRemote = true;
+  protected
+    $whiteListedPlayer = [],
+    $blockedCommands = [],
+    $logFile = null;
 
-	public function onEnable()
-	{
-		@mkdir($this->getDataFolder());
-		$this->saveDefaultConfig();
-		$this->blockConsole = $this->getConfig()->get("block-console");
-		$this->blockRemote = $this->getConfig()->get("block-remote");
-		$this->blockedCommands = explode(",", $this->getConfig()->get("blocked-command"));
-		$this->whiteListedPlayer = explode(",", $this->getConfig()->get("whitelisted-player"));
-		$loggerEnabled = $this->getConfig()->get("log-to-file");
-		if ($loggerEnabled === true) {
-			if (!file_exists($this->getDataFolder() . "logs/")) {
-				mkdir($this->getDataFolder() . "logs/", 0777, true);
-			}
-			$logfilename = $this->getDataFolder() . "logs/Logs_" . date("Y-m-d") . ".log";
-			$this->logFile = fopen($logfilename, "a");
-		}
-		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		$this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
-	}
+  public function onEnable()
+  {
+    @mkdir($this->getDataFolder());
+    $this->saveDefaultConfig();
+    $this->blockConsole = $this->getConfig()->get("block-console");
+    $this->blockRemote = $this->getConfig()->get("block-remote");
+    $this->blockedCommands = explode(",", $this->getConfig()->get("blocked-command"));
+    $this->whiteListedPlayer = explode(",", $this->getConfig()->get("whitelisted-player"));
+    $loggerEnabled = $this->getConfig()->get("log-to-file");
+    if ($loggerEnabled === true) {
+      if (!file_exists($this->getDataFolder() . "logs/")) {
+        mkdir($this->getDataFolder() . "logs/", 0777, true);
+      }
+      $logfilename = $this->getDataFolder() . "logs/Logs_" . date("Y-m-d") . ".log";
+      $this->logFile = fopen($logfilename, "a");
+    }
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+  }
 
-	public function onDisable()
-	{
-		if ($this->logFile !== null) {
-			fclose($this->logFile);
-		}
-	}
+  public function onDisable()
+  {
+    if ($this->logFile !== null) {
+      fclose($this->logFile);
+    }
+  }
 
-	public function isBlocked($cmd)
-	{
-		if (isset($this->blockedCommands)) {
-			$cmd = strtolower($cmd);
-			foreach ($this->blockedCommands as $bcmd) {
-				$bcmd = strtolower($bcmd);
-				if ($cmd === $bcmd) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+  public function isBlocked($cmd)
+  {
+    if (isset($this->blockedCommands)) {
+      $cmd = strtolower($cmd);
+      foreach ($this->blockedCommands as $bcmd) {
+        $bcmd = strtolower($bcmd);
+        if ($cmd === $bcmd) {
+          return true;
+        }
+      }
+    }
 
-	public function isWhiteListed(Player $p)
-	{
-		if (isset($this->whiteListedPlayer)) {
-			$p = $p->getName();
-			$p = strtolower($p);
-			foreach ($this->whiteListedPlayer as $wlp) {
-				$wlp = strtolower($wlp);
-				if ($p === $wlp) {
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+    return false;
+  }
 
-	public function logFile(Player $player, $state, $massage)
-	{
-		if ($this->logFile !== null) {
-			$time = gmdate('Y-m-d h:i:s \G\M\T');
-			$msg = "$time [$state] " . $player->getDisplayName() . '(' . $player->getAddress() . ') : ' . $massage . PHP_EOL;
-			fwrite($this->logFile, "$msg");
-		}
-	}
+  public function isWhiteListed(Player $p)
+  {
+    if (isset($this->whiteListedPlayer)) {
+      $p = $p->getName();
+      $p = strtolower($p);
+      foreach ($this->whiteListedPlayer as $wlp) {
+        $wlp = strtolower($wlp);
+        if ($p === $wlp) {
+          return true;
+        }
+      }
+    }
 
-	public function logCFile($exe, $state, $massage)
-	{
-		if ($this->logFile !== null) {
-			$time = gmdate('Y-m-d h:i:s \G\M\T');
-			$msg = "$time [$state] " . $exe . ' : ' . $massage . PHP_EOL;
-			fwrite($this->logFile, "$msg");
-		}
-	}
+    return false;
+  }
+
+  public function logFile(Player $player, $state, $massage)
+  {
+    if ($this->logFile !== null) {
+      $time = gmdate('Y-m-d h:i:s \G\M\T');
+      $msg = "$time [$state] " . $player->getDisplayName() . '(' . $player->getAddress() . ') : ' . $massage . PHP_EOL;
+      fwrite($this->logFile, "$msg");
+    }
+  }
+
+  public function logCFile($exe, $state, $massage)
+  {
+    if ($this->logFile !== null) {
+      $time = gmdate('Y-m-d h:i:s \G\M\T');
+      $msg = "$time [$state] " . $exe . ' : ' . $massage . PHP_EOL;
+      fwrite($this->logFile, "$msg");
+    }
+  }
 }
